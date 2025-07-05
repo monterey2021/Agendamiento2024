806233f21364bf2cea5c1145d38c6260dc1b34f5
Martín Reyes <rreyes1700@gmail.com>
2025-07-05 07:40:16 -0300

    Actualizando el proyecto

--- index.php
+++ index.php
@@ -10,61 +10,19 @@ if ($_SESSION){
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Iniciar Sesión</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <link rel="shortcut icon" href="img/icono.ico" type="image/x-icon">
+        <title>Iniciar Sesión</title>
         <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-        <style>
-            body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-                padding: 0;
-                background-color: #f8f9fa;
-            }
-            .acceso-usuario {
-                max-width: 330px;
-                padding: 15px;
-                margin: 0 auto;
-            }
-            #sha {
-                background-color: white;
-                max-width: 340px;
-                box-shadow: 0px 0px 18px 0px rgba(48,50,50,0.48);
-                border-radius: 6%;
-            }
-            #logo-acceso {
-                margin: 0px auto 10px;
-                display: block;
-                border-radius: 50%;
-            }
-            .acceso-usuario {
-                font-family: 'Lucida Sans Unicode';
-            }
-            recuperar {
-                /*padding-top: 100px;
-                padding-bottom: 100px;*/
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-                padding: 0;
-                background-color: #f8f9fa;
-            }
-        </style>
+        <link rel="stylesheet" href="css/estiloacceso.css">
     </head>
     <body>
         <div class="container well" id="sha">
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <img src="img/acceso.png" alt="logo-acceso" class="img-responsive" id="logo-acceso"/>
-                </div>
+                <img src="img/logoprincipal.png" alt="logo-acceso" class="img-responsive" id="logo-acceso"/>
             </div>
             <form class="acceso-usuario" action="acceso.php" method="post">
                 <div class="form-group has-feedback">
